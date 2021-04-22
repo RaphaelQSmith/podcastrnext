15bc907b9c62abe7bffc311386f500f029e9fe6f
RaphaelQSmith <49982690+RaphaelQSmith@users.noreply.github.com>
2021-04-22 17:19:56 -0300

    Finishing Player and API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
+import { Header } from "../components/Header";
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props.episodes)
   return (
-    <h1>Hello World, Tchubiruba</h1>
+    <h1>Index</h1>
   )
 }
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+  
+  return {
+    props: {
+      episodes: data,
+    },
+  }
+}
+    
+ 
+ 
