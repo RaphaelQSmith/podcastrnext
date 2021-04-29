2876dc663482379e4fb2b0dd3071387920c52275
RaphaelQSmith <49982690+RaphaelQSmith@users.noreply.github.com>
2021-04-29 01:29:37 -0300

    Episode description styling

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,8 +1,10 @@
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR'
 import { GetStaticPaths, GetStaticProps } from 'next';
-import {useRouter} from 'next/router'
+import Link from 'next/link'
 import { api } from '../../services/api'
+import styles from './episode.module.scss'
+import Image from 'next/image'
 
 type Episode = {
     id: string;
@@ -20,10 +22,33 @@ type EpisodeProps = {
     episode: Episode;
 }
 
-export default function Episode({ episode}: EpisodeProps){
-    const router = useRouter();
+export default function Episode({ episode }: EpisodeProps){
     return (
-        <h1>{episode.id}</h1>
+        <div className={styles.episode}>
+            <div className={styles.thumbnailContainer}>
+                <Link href="/">
+                    <button type='button'>
+                        <img src="/arrow-left.svg" alt="Voltar" />
+                    </button>
+                </Link>
+                <Image 
+                    width={700}
+                    height={160}
+                    src={episode.thumbnail}
+                    objectFit= "cover"
+                />
+                <button type='button'>
+                    <img src="/play.svg" alt="Tocar episódio"/>
+                </button>
+            </div>
+            <header>
+                <h1>{episode.title}</h1>
+                <span>{episode.members}</span>
+                <span>{episode.publishedAt}</span>
+                <span>{episode.durationAsString}</span>
+            </header>
+            <div className={styles.description} dangerouslySetInnerHTML={{__html: episode.description}} />
+        </div>
     )
 }
 
@@ -46,11 +71,13 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
         publishedAt: format(parseISO(data.published_at), 'd MMM yy', {locale: ptBR}),
         duration: Number(data.file.duration),
         // durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
-
+        description: data.description,
         url: data.file.url,
       }
     return{
-        props: {},
+        props: {
+            episode,
+        },
         revalidate: 60 * 60 * 24, //refresh every 24 hours
     }
 }
